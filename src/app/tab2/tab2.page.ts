628546b2dad7f3d8ec801c73ac4aac886be2900a
Carlos Litwiñiuk <repropel78@gmail.com>
2020-11-25 22:36:20 +0100

    WIP Tab 2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService } from '../services/http.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,70 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+
+  experiencias: any[];
+  experiencia: any[];
+  idExperiencia: any[];
+
+  //variables seleccion
+ item: string[];
+ test: string[];
+ 
+
+
+  constructor(private http: HttpService) {
+    //console.log('cargando pagina')
+    this.cargarListadoExperiencias()
+  }
+ 
+
+
+
+  mostrarExperienciaById(){
+
+    
+    //console.log(this.idExperiencia)
+    //console.log(id)
+    this.http.loadExperienciaById(this.idExperiencia).subscribe(
+      (res: any)=>{
+        console.log(res)
+        console.log(res['id']);
+        console.log(res['nombre']);
+        console.log(res['descripcion']);
+        console.log(res['precio']);
+        console.log(res['puntuacion']);
+        console.log(res['fotos'])
+        
+        
+      //this.item = res;
+      this.item = [res['id'],res['nombre'],res['descripcion'],res['precio'],res['puntuacion'],res['fotos']]
+      
+      console.log(this.item)
+      console.log(typeof(this.item)); 
+      
+      console.log(this.item);
+
+      },(error)=>{
+        console.error(error);
+      }
+    );
+
+
+  }
+
+  cargarListadoExperiencias(){
+    
+    this.http.loadExperiencias().subscribe(
+      (res: any)=>{
+        console.log(res)
+        this.experiencias = res;
+
+      },(error)=>{
+        console.error(error);
+      }
+    );
+
+
+  }
 
 }
